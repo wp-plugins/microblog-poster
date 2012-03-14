4ccf8a03aa1031b687bc32b4609a1c7ca11eb4d6
cybperic <cybperic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-14 18:02:39 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/microblog-poster/trunk@519009 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- microblogposter.php
+++ microblogposter.php
@@ -3,7 +3,7 @@
  *
  * Plugin Name: Microblog Poster
  * Description: Easily update your microblog with 'post title + shortened backlink' of your new post.  
- * Version: 1.0
+ * Version: 1.1
  * Author: cybperic
  * Author URI: http://profiles.wordpress.org/users/cybperic/
  *
